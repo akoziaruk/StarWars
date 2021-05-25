ddc0533cc7d7e9121f7288f58df8b45787510f54
Alex Koziaruk <a.koziaruk@noisyminer.com>
2021-05-25 13:33:57 +0300

    Initial Commit

---
+++ StarWarsBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarWarsBrowser
+//
+//  Created by Alexander Koziaruk on 25.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
