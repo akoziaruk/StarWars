3a4da89f2eb804a0ec2c04cc0cac92ae924356cb
Olexander Koziaruk <a.koziaruk@gmail.com>
2021-11-11 16:18:16 +0200

    Added NetworkService unit tests

---
+++ StarWarsBrowserUnitTests/Mocks/URLProtocolMock.swift
@@ -0,0 +1,40 @@
+//
+//  File.swift
+//  StarWarsBrowserUnitTests
+//
+//  Created by Olexander Koziaruk on 11/11/21.
+//
+
+import Foundation
+
+class URLProtocolMock: URLProtocol {
+    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data?))?
+
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+
+    override func startLoading() {
+        guard let handler = URLProtocolMock.requestHandler else {
+            assertionFailure("The handler is not provided!")
+            return
+        }
+
+        do {
+            let (response, data) = try handler(request)
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            if let data = data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+
+    override func stopLoading() { }
+}
---
+++ StarWarsBrowserUnitTests/NetworkServiceTests.swift
@@ -0,0 +1,116 @@
+//
+//  NetworkServiceTests.swift
+//  StarWarsBrowserUnitTests
+//
+//  Created by Olexander Koziaruk on 11/11/21.
+//
+
+import XCTest
+import Combine
+@testable import StarWarsBrowser
+
+class NetworkServiceTests: XCTestCase {
+    private lazy var session: URLSession = {
+        let config = URLSessionConfiguration.ephemeral
+        config.protocolClasses = [URLProtocolMock.self]
+        return URLSession(configuration: config)
+    }()
+    private lazy var networkService = NetworkService(session: session)
+    private let resource = Resource<Categories>.categories()
+    private lazy var categoriesJsonData: Data = {
+        let url = Bundle(for: NetworkServiceTests.self).url(forResource: "Categories", withExtension: "json")
+        guard let resourceUrl = url, let data = try? Data(contentsOf: resourceUrl) else {
+            XCTFail("Failed to create data object from string!")
+            return Data()
+        }
+        return data
+    }()
+    private var cancellables: [AnyCancellable] = []
+
+    override class func setUp() {
+        URLProtocol.registerClass(URLProtocolMock.self)
+    }
+    
+    func test_loadFinishedSuccessfully() {
+        // Given
+        var result: Result<Categories, Error>?
+        let expectation = self.expectation(description: "networkServiceExpectation")
+        URLProtocolMock.requestHandler = { request in
+            let response = HTTPURLResponse(url: self.resource.url, statusCode: 200, httpVersion: nil, headerFields: nil)!
+            return (response, self.categoriesJsonData)
+        }
+        
+        // When
+        networkService.load(resource)
+            .map({ categories -> Result<Categories, Error> in Result.success(categories) })
+            .catch({ error -> AnyPublisher<Result<Categories, Error>, Never> in .just(.failure(error)) })
+            .sink(receiveValue: { value in
+                result = value
+                expectation.fulfill()
+            }).store(in: &cancellables)
+               
+        // Then
+        self.waitForExpectations(timeout: 1.0, handler: nil)
+        guard case .success(let categories) = result else {
+            XCTFail()
+            return
+        }
+        XCTAssertEqual(categories.items.count, 6)
+    }
+    
+    func test_loadFailedWithInternalError() {
+        // Given
+        var result: Result<Categories, Error>?
+        let expectation = self.expectation(description: "networkServiceExpectation")
+        URLProtocolMock.requestHandler = { request in
+            let response = HTTPURLResponse(url: self.resource.url, statusCode: 500, httpVersion: nil, headerFields: nil)!
+            return (response, Data())
+        }
+        
+        // When
+        networkService.load(resource)
+            .map({ categories -> Result<Categories, Error> in Result.success(categories) })
+            .catch({ error -> AnyPublisher<Result<Categories, Error>, Never> in .just(.failure(error)) })
+            .sink(receiveValue: { value in
+                result = value
+                expectation.fulfill()
+            }).store(in: &cancellables)
+               
+        // Then
+        self.waitForExpectations(timeout: 1.0, handler: nil)
+        guard case .failure(let error) = result,
+            let networkError = error as? NetworkError,
+            case NetworkError.dataLoadingError(500, _) = networkError else {
+                XCTFail()
+            return
+        }
+    }
+    
+    func test_loadFailedWithJsonParsingError() {
+        // Given
+        var result: Result<Categories, Error>?
+        let expectation = self.expectation(description: "networkServiceExpectation")
+        URLProtocolMock.requestHandler = { request in
+            let response = HTTPURLResponse(url: self.resource.url, statusCode: 500, httpVersion: nil, headerFields: nil)!
+            return (response, Data())
+        }
+        
+        // When
+        networkService.load(resource)
+            .map({ categories -> Result<Categories, Error> in Result.success(categories) })
+            .catch({ error -> AnyPublisher<Result<Categories, Error>, Never> in .just(.failure(error)) })
+            .sink(receiveValue: { value in
+                result = value
+                expectation.fulfill()
+            }).store(in: &cancellables)
+                    
+        // Then
+        self.waitForExpectations(timeout: 1.0, handler: nil)
+        guard case .failure(let error) = result,
+            let networkError = error as? NetworkError,
+            case NetworkError.dataLoadingError(500, _) = networkError else {
+                XCTFail()
+            return
+        }
+    }
+}
