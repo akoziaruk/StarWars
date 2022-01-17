393c95368a03cdb1aeb65298b7ea732dd8011d65
Alex <a.koziaruk@gmail.com>
2022-01-17 09:53:40 -0500

    Fixed issue when Firebase Bucket failed to return file name with "é" symbol (Padmé Amidala

--- StarWarsBrowser/UseCase/Details/Detail+Bucket.swift
+++ StarWarsBrowser/UseCase/Details/Detail+Bucket.swift
@@ -9,6 +9,13 @@ import Foundation
 
 extension Detailable {
     var bucketImagePath: String {
-        return APIConstants.firebaseBucketPath + imagePath + ".jpg"
+        return APIConstants.firebaseBucketPath + imagePath.normalised + ".jpg"
+    }
+}
+
+/** Firebase Bucket failed to return file name with "é" symbol (Padmé Amidala) */
+fileprivate extension String {
+    var normalised: String {
+        return replacingOccurrences(of: "é", with: "e")
     }
 }
